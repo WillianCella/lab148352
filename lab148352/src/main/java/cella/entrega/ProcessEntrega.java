d40a3740847b00b152b26cd959fcae820866ee38
Willian <willian.cella.wc@gmail.com>
2016-08-11 22:36:25 -0300

    #4 Criado os EJBs com os métodos processar!

---
+++ lab148352/src/main/java/cella/entrega/ProcessEntrega.java
@@ -0,0 +1,11 @@
+package cella.entrega;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class ProcessEntrega {
+
+	public void processarEntrega(Entrega entrega) {
+
+	}
+}
---
+++ lab148352/src/main/java/cella/venda/ProcessVenda.java
@@ -0,0 +1,10 @@
+package cella.venda;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class ProcessVenda {
+	public void processarVenda(Venda venda) {
+
+	}
+}
