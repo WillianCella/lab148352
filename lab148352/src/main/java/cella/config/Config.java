05c7e4a4cda90d2c07f0ac4096b4e490d3421302
Escritório01 <Escritrio01@Escritorio01>
2016-08-18 13:53:23 -0300

    #5 Criado o ServLet Config com as annotation para configurar fila e tópico

---
+++ lab148352/src/main/java/cella/config/Config.java
@@ -0,0 +1,34 @@
+package cella.config;
+
+import java.io.IOException;
+
+import javax.jms.JMSDestinationDefinition;
+import javax.jms.JMSDestinationDefinitions;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@JMSDestinationDefinitions(
+		value = {
+		        @JMSDestinationDefinition(
+		            name = "java:/queue/QueuePedido",
+		            interfaceName = "javax.jms.Queue",
+		            destinationName = "QueuePedido"
+		        ),
+		        @JMSDestinationDefinition(
+		            name = "java:/topic/TopicVenda",
+		            interfaceName = "javax.jms.Topic",
+		            destinationName = "TopicVenda"
+		        )
+		    })
+
+@WebServlet ("config")
+public class Config extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doGet(req, resp);
+	}
+
+}
